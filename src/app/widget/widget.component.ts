9498e76eb2dde8cabd2b77a544f8422943831ea6
kishanmund <kishan.mund@gmail.com>
2022-09-02 00:09:11 +0530

    +

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -7,33 +7,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetComponent implements OnInit {
 
-  offerDetails = [
+  widgetRows =[
     {
-      header:"Offer Extended on",
-      date:"21 Jul 2020",
-      isIcon:false
+      header:"Offer Extended",
+      profileURL:"./../../assets/images/profile-pic.jpeg",
+      name:"Anil Kapoor",
+      position:"Director, Piramal Enterprises",
+      location:"Mumbai",
+      offerDetails : [
+        {
+          header:"Offer Extended on",
+          date:"21 Jul 2020",
+          isIcon:false
+        },
+        {
+          header:"Offer Accepted on",
+          date:"23 Jul 2020",
+          isIcon:false
+        },
+        {
+          header:"Date of Joining",
+          date:"1 Sep 2020",
+          isIcon:false
+        },
+        {
+          header:"Status",
+          date:"",
+          isIcon:true,
+          iconName:"approved"
+        },
+        {
+          header:"Background Verification",
+          date:"",
+          isIcon:true,
+          iconName:"approved"
+        },
+      ],
+      docName:"DOCUMENT",
+      referenceCheck:false,
+      references:[]
     },
     {
-      header:"Offer Accepted on",
-      date:"23 Jul 2020",
-      isIcon:false
-    },
-    {
-      header:"Date of Joining",
-      date:"1 Sep 2020",
-      isIcon:false
-    },
-    {
-      header:"Status",
-      date:"",
-      isIcon:false
-    },
-    {
-      header:"Background Verification",
-      date:"",
-      isIcon:false
-    },
+      header:"Dropped",
+      profileURL:"./../../assets/images/profile-pic.jpeg",
+      name:"Shridhar Joshi",
+      position:"Director, Piramal Enterprises",
+      location:"Mumbai",
+      offerDetails : [
+        {
+          header:"No Hire",
+          date:"1 Jul 2018",
+          isIcon:true,
+          iconName:"dislike"
+        },
+        {
+          header:"No Hire",
+          date:"1 Jul 2018",
+          isIcon:true,
+          iconName:"dislike"
+        },
+        {
+          header:"",
+          date:"1 Jul 2018",
+          isIcon:true,
+          iconName:"calendar"
+        },
+        {
+          header:"",
+          date:"1 Jul 2018",
+          isIcon:true,
+          iconName:"calendar"
+        },
+        {
+          header:"",
+          date:"1 Jul 2018",
+          isIcon:true,
+          iconName:"calendar"
+        },
+      ],
+      docName:"DOCUMENT",
+      referenceCheck:true,
+      references:[
+        7,3
+      ]
+    }
   ]
+
+  
 
   constructor() { }
 
